100b5c3c26f98deb41fc091f467628c410d9b1c5
TUSHAR GODHVANI <101061942+tushar143143@users.noreply.github.com>
2024-03-03 17:59:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import AddTodo from './component/AddTodo'
+import Todos from './component/Todos'
+function App() {
+  
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+
+    </>
+  )
+}
+
+export default App
